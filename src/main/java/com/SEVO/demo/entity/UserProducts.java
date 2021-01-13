6150baa5d1fe2899e6209ecf03266c8ea4551cac
Your Name Here <your_email@youremail.com>
2021-01-13 15:38:13 +0100

    git g

--- src/main/java/com/SEVO/demo/entity/UserProducts.java
+++ src/main/java/com/SEVO/demo/entity/UserProducts.java
@@ -26,11 +26,11 @@ public class UserProducts {
 	@Column(name = "id")
 	private int id;
 	
-	@ManyToOne(cascade = CascadeType.ALL)
+	@ManyToOne(cascade = CascadeType.DETACH)
 	@JoinColumn(name = "userproduct_id")
 	private Product userproductId;
 	
-	@ManyToOne(cascade = CascadeType.ALL)
+	@ManyToOne(cascade = CascadeType.DETACH)
 	@JoinColumn(name = "owner_id")
 	private User ownerId;
 	
